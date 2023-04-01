40fa1bcd0f25c07e6e4c5f0b0c991f815c534336
Holger H <holgheiss@gmail.com>
2023-04-01 12:47:36 +0200

    fix env vars

--- app/imports/api/env.js
+++ app/imports/api/env.js
@@ -1,7 +1,7 @@
-MEDIASERVER_ADDRESS = process.env.CYBORGMASTER_MEDIASERVER_ADDRESS || "http://localhost:3000"
-MEDIASERVER_PATH    = process.env.CYBORGMASTER_MEDIASERVER_PATH || "media/"
-LOCAL_MEDIA_PATH    = process.env.CYBORGMASTER_LOCAL_MEDIA_PATH || "/Users/holger/Documents/Projekte/360vr/code/360vr/data/media/"
-LOCAL_HLS_PATH      = process.env.CYBORGMASTER_LOCAL_MEDIA_PATH || "/Users/holger/Documents/Projekte/360vr/code/360vr/data/hls/"
+MEDIASERVER_ADDRESS = process.env.MEDIASERVER_ADDRESS || "http://localhost:3000"
+MEDIASERVER_PATH    = process.env.MEDIASERVER_PATH || "media/"
+LOCAL_MEDIA_PATH    = process.env.LOCAL_MEDIA_PATH || "/Users/holger/Documents/Projekte/360vr/code/360vr/data/media/"
+LOCAL_HLS_PATH      = process.env.LOCAL_MEDIA_PATH || "/Users/holger/Documents/Projekte/360vr/code/360vr/data/hls/"
 
 console.log("mediaserver_address:", MEDIASERVER_ADDRESS)
 console.log("mediaserver_path:", MEDIASERVER_PATH)
